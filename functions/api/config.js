2a76ce940e8264daf087c68b845934e704800136
ikhsan <email.github.ikhsanrofiqi60@gmail.com>
2025-07-23 09:10:38 +0700

    Menata ulang struktur folder untuk Cloudflare

--- functions/api/config.js
+++ functions/api/config.js
@@ -1,17 +1,21 @@
-// functions/config.js
-export default {
-  async fetch(request, env, ctx) {
-    const clientConfig = {
-      apiKey: env.VITE_FIREBASE_API_KEY,
-      authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
-      projectId: env.VITE_FIREBASE_PROJECT_ID,
-      storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      appId: env.VITE_FIREBASE_APP_ID,
-    };
+// functions/api/config.js
 
-    return new Response(JSON.stringify(clientConfig), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  },
-};
+// Fungsi onRequest adalah format yang benar untuk Cloudflare Pages
+export async function onRequest(context) {
+  // 'context.env' berisi environment variables Anda
+  const { env } = context;
+
+  const clientConfig = {
+    apiKey: env.VITE_FIREBASE_API_KEY,
+    authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.VITE_FIREBASE_APP_ID,
+  };
+
+  // Mengembalikan Response baru, sama seperti sebelumnya
+  return new Response(JSON.stringify(clientConfig), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
